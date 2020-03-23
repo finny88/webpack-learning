5d7f7c2097ae59edecfcf99b909aa629af7f7466
e.finogeev <e.finogeev@iteco-inno.ru>
2020-03-23 21:39:20 +0300

    Lazy Loading

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
 import _ from "lodash";
-import printMe from "./print.js";
-// import "./styles.css";
-
-console.log(process.env.NODE_ENV);
 
 function component() {
   const element = document.createElement("div");
-  const btn = document.createElement("button");
 
+  const button = document.createElement("button");
+  const br = document.createElement("br");
+
+  button.innerHTML = "Click me and look at the console!";
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.appendChild(br);
+  element.appendChild(button);
 
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e =>
+    import(/* webpackChunkName: "print" */ "./print").then(module => {
+      const print = module.default;
 
-  element.appendChild(btn);
+      print();
+    });
 
   return element;
 }
 
-let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept("./print.js", function() {
-    console.log("Accepting the updated printMe module!");
-    document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click handler
-    document.body.appendChild(element);
-  });
-}
+document.body.appendChild(component());
--- src/print.js
+++ src/print.js
@@ -1,3 +1,7 @@
-export default function print(text) {
-  console.log("Updating print3.js...");
-}
+console.log(
+  "The print.js module has loaded! See the network tab in dev tools..."
+);
+
+export default () => {
+  console.log('Button Clicked: Here\'s "some text"!');
+};
